c92f006701b462afcbac1519930f4d5610779110
tolik1106 <tolik1106@i.ua>
2019-03-13 16:09:05 +0200

    HW05_fix_hsqlDBjdbcRepository_servlet_SpringMain

---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/HSQLDBJdbcMealRepositoryImpl.java
@@ -0,0 +1,24 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Repository
+@Profile(Profiles.HSQL_DB)
+public class HSQLDBJdbcMealRepositoryImpl extends AbstractJdbcMealRepository {
+
+    public HSQLDBJdbcMealRepositoryImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected Object getConvertedDateTime(LocalDateTime dateTime) {
+        return Timestamp.valueOf(dateTime);
+    }
+}
---
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/PostgresJdbcMealRepositoryImpl.java
@@ -0,0 +1,23 @@
+package ru.javawebinar.topjava.repository.jdbc;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.Profiles;
+
+import java.time.LocalDateTime;
+
+@Repository
+@Profile(Profiles.POSTGRES_DB)
+public class PostgresJdbcMealRepositoryImpl extends AbstractJdbcMealRepository {
+
+    public PostgresJdbcMealRepositoryImpl(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        super(jdbcTemplate, namedParameterJdbcTemplate);
+    }
+
+    @Override
+    protected Object getConvertedDateTime(LocalDateTime dateTime) {
+        return dateTime;
+    }
+}
